b7fd3793497a75af59cd7048bc150749dc5c76f0
mii288 <atteeen@gmail.com>
2020-02-05 07:52:10 +0900

    fix: duplicate p5 canvas

--- src/components/p5.tsx
+++ src/components/p5.tsx
@@ -3,14 +3,22 @@ import P5Instance from 'p5'
 
 /* eslint-disable-next-line */
 const P5: React.SFC<{ sketch: (...args: any[]) => any }> = props => {
-  const [, setP5] = useState<P5Instance>()
+  const [canvas, setP5] = useState<P5Instance>()
   const wrapper = useRef(null)
 
   useEffect(() => {
     setP5(new P5Instance(props.sketch, wrapper.current || undefined))
   }, [props.sketch])
 
-  return <div ref={wrapper} style={{ textAlign: 'center' }}/>
+  // fix duplicate p5 canvas
+  useEffect(
+    () => () => {
+      if (canvas) canvas.remove()
+    },
+    [canvas]
+  )
+
+  return <div ref={wrapper} style={{ textAlign: 'center' }} />
 }
 
 export default P5
