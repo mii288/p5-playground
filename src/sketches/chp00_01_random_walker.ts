265e116f9a9fe14f736914ae29806264702c6ed6
mii288 <atteeen@gmail.com>
2020-02-05 07:06:17 +0900

    feat(c0_01): update

--- src/sketches/chp00_01_random_walker.ts
+++ src/sketches/chp00_01_random_walker.ts
@@ -1,15 +1,46 @@
 import p5 from 'p5'
 
 export default (p: p5) => {
+  let walker: Walker
+  class Walker {
+    x: number
+    y: number
+
+    constructor() {
+      this.x = p.width / 2
+      this.y = p.height / 2
+    }
+
+    render() {
+      p.stroke(0)
+      p.point(this.x, this.y)
+    }
+
+    step() {
+      const choice = p.floor(p.random(4))
+      if (choice === 0) {
+        this.x++
+      } else if (choice === 1) {
+        this.x--
+      } else if (choice === 2) {
+        this.y++
+      } else {
+        this.y--
+      }
+
+      // this.x = p.constrain(this.x, 0, p.width - 1)
+      // this.y = p.constrain(this.y, 0, p.height - 1)
+    }
+  }
+
   p.setup = () => {
-    p.createCanvas(300, 300)
+    p.createCanvas(640, 360)
+    walker = new Walker()
+    p.background(127)
   }
 
   p.draw = () => {
-    p.background(255)
-    if (p.mouseX === 0 && p.mouseY === 0) return
-    p.fill(255, 0, 0)
-    p.noStroke()
-    p.ellipse(p.mouseX, p.mouseY, 100, 100)
+    walker.step()
+    walker.render()
   }
 }
