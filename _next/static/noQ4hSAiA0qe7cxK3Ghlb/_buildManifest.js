4632a65322f0fe53d5b64654d1151f7e8dc83d7f
mii288 <mii288@users.noreply.github.com>
2021-01-25 14:25:12 +0000

    Automated deployment: Mon Jan 25 14:25:12 UTC 2021 107a0ff8a3cc329ee5daea7c10824bf1e64a8128

---
+++ _next/static/noQ4hSAiA0qe7cxK3Ghlb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/post":[a,b]}}("static\u002Fchunks\u002F880d6526.15cf6e41c6fa3fa84cb0.js","static\u002Fchunks\u002F34b57472bd7a0ce7c7f55ee05536aabea6b6ddee.ae76f951302196ae15f4.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
